129f96932b611eaef9f3bf377c3a71c69e881cd2
briansomogyi <brian.somogyi@emanuel.ro>
2024-05-23 12:56:29 +0300

    Done some changes

---
+++ fazan.js
@@ -0,0 +1,73 @@
+class Button {
+    #x;
+    #y;
+    #w;
+    #h;
+    #text;
+    #color;
+
+    constructor(x, y, w, h, text, color) {
+        this.#x = x;
+        this.#y = y;
+        this.#w = w;
+        this.#h = h;
+        this.#text = text;
+        this.#color = color;
+    }
+
+    draw() {
+        fill(this.#color);
+        rect(this.#x, this.#y, this.#w, this.#h);
+        fill("black");
+        textSize(30);
+        text(this.#text, this.#x + 8, this.#y + 35);
+
+    }
+
+    click(letter) {
+        if (mouseX >= this.#x &&
+            mouseX <= this.#x + this.#w &&
+            mouseY >= this.#y &&
+            mouseY <= this.#y + this.#h) {
+            // console.log(`${this.#text} button was clicked.`);
+            letter.startCounting();
+        }
+    }
+
+}
+class Letter {
+    #letter;
+    #speed;
+    #color;
+    #x;
+    #y;
+    constructor(letter, speed, color, x, y) {
+        this.#letter = letter;
+        this.#speed = speed;
+        this.#color = color;
+        this.#x = x;
+        this.#y = y;
+    }
+
+    draw() {
+        fill(this.#color);
+        textSize(50);
+        text(this.#letter, this.#x, this.#y);
+    }
+
+    startCounting() {
+        setInterval(this.nextLetter, this.#speed, this);
+    }
+
+    stopCounting() {
+
+    }
+
+    nextLetter(letter) {
+        let letterCode = letter.#letter.charCodeAt() + 1;
+        if (letterCode > "Z".charCodeAt()) {
+            letterCode = "A".charCodeAt();
+        }
+        letter.#letter = String.fromCharCode(letterCode);
+    }
+}
--- script.js
+++ script.js
@@ -1,7 +1,20 @@
+const buttonStart = new Button(200, 300, 40, 20, "Start", "green");
+const buttonStop = new Button(300, 300, 40, 20, "Stop", "red");
+const letter = new Letter("A", 200, "red", 100, 300);
+
 function setup() {
     createCanvas(400, 400);
 }
 
 function draw() {
+    clear();
     background(220);
+    buttonStart.draw();
+    buttonStop.draw();
+    letter.draw();
+}
+
+function mouseClicked() {
+    buttonStart.click(letter);
+    buttonStop.click(letter);
 }
